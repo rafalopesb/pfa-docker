d3a0812527afecf17bc97ede4a608ff50506e8c7
Rafael Lopes Bastos <rafalopesb@hotmail.com>
2021-04-26 12:08:47 -0300

    Ädicionado readme e alterado script

--- docker/run.sh
+++ docker/run.sh
@@ -2,10 +2,10 @@ docker build -t rafalopesb/mysql ./mysql
 docker run -d -e MYSQL_ROOT_PASSWORD=root --network minharede --name mysql rafalopesb/mysql   
 
 docker build -t rafalopesb/php ./php
-docker run -d -v $(pwd)/laravel:/var/www/html --network minharede --name phpcontainer rafalopesb/php
+docker run -d -v $(pwd)../../..:/var/www/html --network minharede --name phpcontainer rafalopesb/php
 
 docker build -t rafalopesb/nginx ./nginx
-docker run -d --network minharede --name nginx --rm -v $(pwd)/laravel/:/var/www/html -p 80:80 rafalopesb/nginx 
+docker run -d --network minharede --name nginx --rm -v $(pwd)../../..:/var/www/html -p 80:80 rafalopesb/nginx 
 
 sleep 5
 
