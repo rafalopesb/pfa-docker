938e7e5a119ab744c39c6f637f73873ef85f7909
Rafael Lopes Bastos <rafalopesb@hotmail.com>
2021-04-30 16:22:30 -0300

    Alterado o run para adicionar o env

--- docker/php/run.sh
+++ docker/php/run.sh
@@ -1,3 +1,4 @@
 /var/www/html/pfa-fullcycle/artisan migrate --force
 /var/www/html/pfa-fullcycle/artisan db:seed --class=CursosSeeder --force
+cp /var/www/html/pfa-fullcycle/.env.example /var/www/html/pfa-fullcycle.env 
 chmod 777 /var/www/html/pfa-fullcycle/storage -R
